4c9eb49eb78d88db74020e88fc6cb3186c156d55
VinayakNSutar <58587997+VinayakNSutar@users.noreply.github.com>
2020-04-06 17:32:03 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,17 @@
-days = [("Sun",0), ("mon",1), ("tue",2), ("wed",3), ("thus",4), ("fri",5), ("satu",6)]
-i = int(input("enter starting day number: "))
-j = int(input("length of the stay: "))
-for day, number in days:
-    if j < 7:
-        x = i + j   #if j<7 then add it with i
-        x= x - 7    #then minus 7 from addition will give you the day 
-        if number == x:
-            print(day)
-    if j > 7:
-        while j > 7:
-            y= j%7
-        print(y)
-        i = i + y
-        if number == i:
-            print(day)
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
